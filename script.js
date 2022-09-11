cf1fa48e2c8d693bb5155e0dacac1b2abb44ea29
Jatin-CI0002 <jatin@caizin.com>
2022-09-12 00:34:24 +0530

    final commit

--- script.js
+++ script.js
@@ -1,105 +1,109 @@
 let startButton1 = document.getElementById("start1");
-let stopButton1 = document.getElementById("stop1");
-let resetButton1 = document.getElementById("reset1");
-
 let startButton2 = document.getElementById("start2");
+
+let stopButton1 = document.getElementById("stop1");
 let stopButton2 = document.getElementById("stop2");
+
+let resetButton1 = document.getElementById("reset1");
 let resetButton2 = document.getElementById("reset2");
-
-let minute1 = document.getElementById("minute1"); 
-let second1 = document.getElementById("second1");
-let mSecond1 = document.getElementById("mSecond1");
-
-let minute2 = document.getElementById("minute2");
-let second2 = document.getElementById("second2");
-let mSecond2 = document.getElementById("mSecond2");
+let display1 = document.getElementById("display1");
+let display2 = document.getElementById("display2");
 
 let interval1;
 let interval2;
-let hr1 = min1 = sec1 = ms1 = "0" + 0;
-let hr2 = min2 = sec2 = ms2 = "0" + 0;
 
-startButton1.addEventListener("click", () => {
-  interval1 = setInterval(() => {
-    ms1++;
-    if (ms1 == 100) {
-      sec1++;
-      sec1 = sec1 < 10 ? "0" + sec1 : sec1;
-      ms1 = "0" + 0;
-    }
+let obj1 = {
+  "min": 00,
+  "sec": 00,
+  "ms": 00
+};
 
-    if (sec1 == 60) {
-      min1++;
-      min1 = min1 < 10 ? "0" + min1 : min1;
-      sec1 = "0" + 0;
-    }
+let obj2 = {
+  "min": 00,
+  "sec": 00,
+  "ms": 00
+};
 
-    if (min1 == 60) {
-      hr1++;
-      hr1 = hr1 < 10 ? "0" + hr1 : hr1;
-      min1 = "0" + 0;
-    }
-    displayTime1();
-  }, 10);
-});
+let hr1 = (min1 = sec1 = ms1 = "0" + 0);
+let hr2 = (min2 = sec2 = ms2 = "0" + 0);
 
-stopButton1.addEventListener("click", () => {
-  clearInterval(interval1);
-});
+startButton1.onclick = function () {
+  interval1 = start("display1", obj1.min, obj1.sec, obj1.ms, interval1);
+};
 
-resetButton1.addEventListener("click",()=>{
-    clearInterval(interval1);
-    hr1 = min1 = sec1 = ms1 = "0" + 0;
-    displayTime1();
-})
+startButton2.onclick = function () {
+  interval2 = start("display2", obj2.min, obj2.sec, obj2.ms, interval2);
+};
 
-function displayTime1() {
-  minute1.innerHTML = min1;
-  second1.innerHTML = sec1;
-  mSecond1.innerHTML = ms1;
+stopButton1.onclick = function () {
+  console.log("hello1");
+  let str = display1.innerText;
+  let temp = str.split(":");
+  obj1 = {
+    min: parseInt(temp[0]),
+    sec: parseInt(temp[1]),
+    ms: parseInt(temp[2])
+  };
+  stop(interval1);
+};
+
+stopButton2.onclick = function () {
+  console.log("hello2");
+  let str = display1.innerText;
+  let temp = str.split(":");
+  obj2 = {
+    min: parseInt(temp[0]),
+    sec: parseInt(temp[1]),
+    ms: parseInt(temp[2])
+  };
+  stop(interval2);
+};
+
+resetButton1.onclick = function (){
+  reset(obj1, interval1, display1);
 }
 
-
-// For timer 2
-
-startButton2.addEventListener("click", () => {
-  interval2 = setInterval(() => {
-    ms2++;
-    if (ms2 == 100) {
-      sec2++;
-      sec2 = sec2 < 10 ? "0" + sec2 : sec2;
-      ms2 = "0" + 0;
-    }
-
-    if (sec2 == 60) {
-      min2++;
-      min2 = min2 < 10 ? "0" + min2 : min2;
-      sec2 = "0" + 0;
-    }
-
-    if (min2 == 60) {
-      hr2++;
-      hr2 = hr2 < 10 ? "0" + hr2 : hr2;
-      min2 = "0" + 0;
-    }
-    displayTime2();
-  }, 10);
-});
-
-stopButton2.addEventListener("click", () => {
-  clearInterval(interval2);
-});
-
-resetButton2.addEventListener("click",()=>{
-    clearInterval(interval2);
-    hr2 = min2 = sec2 = ms2 = "0" + 0;
-    displayTime2();
-})
-
-function displayTime2() {
-  minute2.innerHTML = min2;
-  second2.innerHTML = sec2;
-  mSecond2.innerHTML = ms2;
+resetButton2.onclick = function (){
+  reset(obj2, interval2, display2);
 }
 
+function start(element, min, sec, ms, interval) {
+  let display = document.getElementById(element);
+  interval = setInterval(() => {
+    ms++;
+    if (ms == 100) {
+      sec++;
+      sec = sec < 10 ? "0" + sec : sec;
+      ms = "0" + 0;
+    }
 
+    if (sec == 60) {
+      min++;
+      min = min < 10 ? "0" + min : min;
+      sec = "0" + 0;
+    }
+
+    if (min == 60) {
+      hr++;
+      hr = hr < 10 ? "0" + hr : hr;
+      min = "0" + 0;
+    }
+    let value = `${min} : ${sec} : ${ms}`;
+    display.innerHTML = value;
+  }, 10);
+  
+  return interval;
+}
+
+function stop(interval) {
+  console.log(interval);
+  clearInterval(interval);
+}
+
+function reset(obj, interval, display){
+  obj.min = 0;
+  obj.sec = 0;
+  obj.ms = 0;
+  display.innerHTML = "0 : 0 : 0";
+  clearInterval(interval);
+}
